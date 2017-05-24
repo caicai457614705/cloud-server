2d2d40c329709d3704971c86477ab9282f486c71
faker <wangzhipeng2817@tops001.com>
2017-05-24 14:40:39 +0800

    去掉无用文件 ,  .gitignore修改

--- src/main/java/com/example/demo/CloudServerApplication.java
+++ src/main/java/com/example/demo/CloudServerApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class CloudServerApplication {
 
 	public static void main(String[] args) {
